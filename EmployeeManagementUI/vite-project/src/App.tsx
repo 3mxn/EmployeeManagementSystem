cf924633372a66b0fb380be4971868b05e803ef9
3mxn <eeman.shamim@breakthru.pk>
2026-07-28 07:10:05 -0700

    t rebase --continuegit rebase --continuegit rebase --continuupdate the changes done in files Organize API and UI folders properly

--- EmployeeManagementUI/vite-project/src/App.tsx
+++ EmployeeManagementUI/vite-project/src/App.tsx
@@ -1,11 +1,11 @@
-// import EmployeeList from "./Components/EmployeeList";
-// function App() {
-//   return (
-//     <div className="container mt-4">
-//       <h2>Employee Management</h2>
-//       <EmployeeList/>
-//     </div>
-//   );
-// }
+import EmployeeList from "./Components/EmployeeList";
+function App() {
+  return (
+    <div className="container mt-4">
+      <h2>Employee Management</h2>
+      <EmployeeList/>
+    </div>
+  );
+}
 
-// export default App;
+export default App;
--- EmployeeManagementUI/vite-project/src/Components/EmployeeList.tsx
+++ EmployeeManagementUI/vite-project/src/Components/EmployeeList.tsx
@@ -1,21 +1,48 @@
-// import {useEffect,useState} from "react";
+import {useEffect,useState} from "react";
 
-// type Employee={
-//     employeeId:number;
-//     name:string;
-//     email:string;
-//     role:string
-// };
-// function EmployeeList(){
-//     const[employees,setEmployees]=useState<Employee[]>([]);
-//     const loadEmployees=()=>{
-//       fetch("https://localhost:/employees")
-//     };
-//     return(
-//         <div>
-//             <h3>Employee List</h3>
-//         </div>
-//     );
-// }
+type Employee={
+    employeeId:number;
+    name:string;
+    email:string;
+    role:string
+};
+function EmployeeList(){
+    const[employees,setEmployees]=useState<Employee[]>([]);
+    const loadEmployees=()=>{
+      fetch("https://localhost:7116/employees")
+      .then((response)=>response.json())
+      .then((data)=>setEmployees(data));
+    };
+    useEffect(()=>{
+        loadEmployees();
+    },[])
+    return(
+        <div>
+            <h3>Employee List</h3>
+            <table className="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {employees.map((employee)=>(
+                        <tr key={employee.employeeId}>
+                            <td>{employee.employeeId}</td>
+                            <td>{employee.name}</td>
+                            <td>{employee.email}</td>
+                            <td>{employee.role}</td>
 
-// export default EmployeeList;
+                        </tr>
+
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default EmployeeList;
